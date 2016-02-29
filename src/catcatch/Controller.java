b4a86bf81a664ccfa2d9c84805e7a30c6ef6f26f
kjrya <kjrya@104.39.67.134>
2016-02-29 15:39:13 -0500

    Added an extends JFrame and comments on how the controller will function from now on

--- src/catcatch/Controller.java
+++ src/catcatch/Controller.java
@@ -6,16 +6,23 @@
 
 package catcatch;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author Laura
  */
-public class Controller {
+public class Controller extends JFrame{
     final private int WIDTH = 0;
     final private int HEIGHT = 0;
     
     
     public Controller(){
+        
+        //Controller extends JFrame
+        //On controller initialization we need to have it creat a JFrame and a 
+        //JPanel so then we will have one parent JPanel to add other elements to
+        
         StartPanel startPanel = new StartPanel(this);
         startPanel.setVisible(true);
         startPanel.setLocationRelativeTo(null);
