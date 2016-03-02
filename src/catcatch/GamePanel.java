da4f84b9411a403cb29e1927b3028081dd1fbc17
kjrya <kjrya@104.39.67.134>
2016-03-02 14:37:12 -0500

    Making a little more progress

--- src/catcatch/GamePanel.java
+++ src/catcatch/GamePanel.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -21,9 +24,10 @@ import javax.swing.border.LineBorder;
  *
  * @author kjrya
  */
-public class GamePanel extends javax.swing.JPanel{
+public class GamePanel extends javax.swing.JPanel implements ActionListener, KeyListener{
     
-    private Timer time;
+    private Timer gameTimer;
+    private Timer blockTimer;
     private Player player;
     private ArrayList<CatBlock> catBlocks;
     private ArrayList<FireBlock> fireBlocks;
@@ -49,10 +53,31 @@ public class GamePanel extends javax.swing.JPanel{
         }
         this.add(gui);    
     }
-    /*
-    public void paintComponent(Graphics g){
-        super.paintComponents(g);
-        g.fillRect(200, 62, 30, 10);
+5    
+    public void actionPerformed(ActionEvent event){
+        
+        //sets the object to the source of whatever event occurs
+        Object obj = event.getSource();
+        
+        if(obj == gameTimer)
+        {
+            //updates panel as per gameTimer
+            this.repaint();
+        } 
+        else if(obj == blockTimer)
+        {
+            //adds block objects to their respective arrays
+            //we'll need to change the constructors for each
+            catBlocks.add(new CatBlock(400,400, this));
+            fireBlocks.add(new FireBlock(400,400, this));
+        }
     }
-    */
+    
+    public void paintcomponent(Graphics g){
+        super.paintComponent(g);
+        
+        g.clearRect(0,0, this.getWidth(), this.getHeight());
+        background.paintcomponent(g);
+    }
+    
 }
